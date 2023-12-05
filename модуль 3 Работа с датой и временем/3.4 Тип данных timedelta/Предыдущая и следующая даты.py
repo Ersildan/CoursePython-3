542ab564d6ad4c3bda1320b1b2a02c5960876088
Andrey Svistunov <120823381+Ersildan@users.noreply.github.com>
2023-12-05 22:51:05 +0300

    +1

--- модуль 3 Работа с датой и временем/3.4 Тип данных timedelta/Предыдущая и следующая даты.py
+++ модуль 3 Работа с датой и временем/3.4 Тип данных timedelta/Предыдущая и следующая даты.py
@@ -5,3 +5,5 @@ yesterday = my_date - timedelta(days=1)
 tomorrow =  my_date + timedelta(days=1)
 
 print(datetime.strftime(yesterday, "%d.%m.%Y"), datetime.strftime(tomorrow, "%d.%m.%Y"), sep='\n')
+
+'''text''

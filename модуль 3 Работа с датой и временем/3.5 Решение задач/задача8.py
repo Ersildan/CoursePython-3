87e704d00d30f22801d0543c4833da65cb682a1c
Andrey Svistunov <120823381+Ersildan@users.noreply.github.com>
2023-12-18 23:46:32 +0300

    +1

---
+++ модуль 3 Работа с датой и временем/3.5 Решение задач/задача8.py
@@ -0,0 +1,11 @@
+from datetime import datetime as dt, timedelta as td
+
+dx = dt(day= 8, month= 11, year= 2022, hour=12)
+dy = dt.strptime(input(), '%d.%m.%Y %H:%M')
+
+x = dx - dy
+xts = x.total_seconds()
+xdays = xts / 86400
+xhours = xts / 1200
+xminutes = xts / 60
+

bc08f9b74bc8f9c335b4aa56716700169cf4f694
Andrey Svistunov <11free11@mail.ru>
2024-02-04 16:47:11 +0300

    Задачи на csv файлы

--- C Модуль 4 Работа с файлами/2 Работа с csv файлами/Проще некуда.py
+++ C Модуль 4 Работа с файлами/2 Работа с csv файлами/Проще некуда.py
@@ -1,1 +1,22 @@
-"Задача на csv 02.02.24"
+import csv
+
+
+def condense_csv(filename, id_name):
+    with open(filename, encoding='utf-8') as file:
+        data = list(csv.reader(file))
+        res = dict()
+        for i in data:
+            res[id_name] = res.get(id_name, []) + [i[0]]
+            res[i[1]] = res.get(i[1], []) + [i[2]]
+
+        res[id_name] = sorted(set(res.get(id_name)))
+        matrix = [_ for _ in res.values()]
+        row = [_ for _ in zip(*matrix)]
+
+        with open('condensed.csv', 'w', encoding='utf-8', newline='') as f:
+            writer = csv.writer(f, delimiter=',', quoting=csv.QUOTE_NONE)
+            writer.writerow([_ for _ in res.keys()])
+            writer.writerows(row)
+
+
+print(condense_csv('data2.csv', id_name="ID"))

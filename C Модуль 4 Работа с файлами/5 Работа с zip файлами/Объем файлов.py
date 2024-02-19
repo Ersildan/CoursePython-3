8fcfb7993e4fa87c39121bc0547ffeb403647325
Andrey Svistunov <11free11@mail.ru>
2024-02-19 20:32:48 +0300

    Working with a zip file.

--- C Модуль 4 Работа с файлами/5 Работа с zip файлами/Объем файлов.py
+++ C Модуль 4 Работа с файлами/5 Работа с zip файлами/Объем файлов.py
@@ -1,1 +1,8 @@
 from zipfile import ZipFile
+
+with ZipFile('workbook.zip') as zp:
+    lst = sum([i.file_size for i in zp.infolist()])
+    lst_ = sum([i.compress_size for i in zp.infolist()])
+
+    print(f"Объем исходных файлов: {lst} байт(а)")
+    print(f"Объем сжатых файлов: {lst_} байт(а)")

0bc2043c8753d7012d4caa382d053c408f95003a
Andrey Svistunov <11free11@mail.ru>
2024-03-04 22:02:35 +0300

    Working with a zip file.

--- C Модуль 4 Работа с файлами/5 Работа с zip файлами/Структура архива.py
+++ C Модуль 4 Работа с файлами/5 Работа с zip файлами/Структура архива.py
@@ -1,3 +1,17 @@
 from zipfile import ZipFile
+import json
+import os.path
 
-with ZipFile('desktop.zip') as zp:
+
+def foo():
+    pass
+
+
+with ZipFile('test.zip') as zip_file:
+    for el in zip_file.infolist():
+        if not el.is_dir():
+            print(el.filename.split("/")[-1])
+        else:
+            print(f'{el.filename}')
+
+

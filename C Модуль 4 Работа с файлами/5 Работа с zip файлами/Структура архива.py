951238b4dfa8dedb22b9b9f5be50751e98744066
Andrey Svistunov <11free11@mail.ru>
2024-03-08 13:29:13 +0300

    Work with a zipfile

--- C Модуль 4 Работа с файлами/5 Работа с zip файлами/Структура архива.py
+++ C Модуль 4 Работа с файлами/5 Работа с zip файлами/Структура архива.py
@@ -1,17 +1,24 @@
 from zipfile import ZipFile
-import json
-import os.path
 
 
-def foo():
-    pass
+def foo(size):
+    if size < 1000:
+        return f'{size} B'
+    elif 1000 <= size < 1000000:
+        return f'{round(size / 1024)} KB'
+    elif 1000000 <= size < 1000000000:
+        return f'{round(size / 1048576)} MB'
+    else:
+        return f'{round(size / 1073741824)} GB'
 
 
 with ZipFile('test.zip') as zip_file:
-    for el in zip_file.infolist():
-        if not el.is_dir():
-            print(el.filename.split("/")[-1])
+    info = zip_file.infolist()
+    for el in info:
+        name = el.filename
+        wt = el.file_size
+        n_split = len(name[:-1].split('/')) - 1
+        if el.is_dir():
+            print(f"{'  ' * n_split}{name[:-1].split('/')[-1]}")
         else:
-            print(f'{el.filename}')
-
-
+            print(f"{'  ' * n_split}{name.split('/')[-1]} {foo(wt)}")

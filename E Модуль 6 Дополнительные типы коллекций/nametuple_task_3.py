2b49dd0ef46732d17a28dbbf43ccd79058137c77
Andrey Svistunov <11free11@mail.ru>
2024-04-16 23:36:38 +0300

    Module nametuple python

--- E Модуль 6 Дополнительные типы коллекций/nametuple_task_3.py
+++ E Модуль 6 Дополнительные типы коллекций/nametuple_task_3.py
@@ -1,12 +1,14 @@
 import csv
 from collections import namedtuple
+from datetime import datetime as dt
 
 with open('meetings.csv', encoding="utf-8") as file:
     friends = namedtuple('friends', ['surname', 'name', 'meeting_date', 'meeting_time'])
-
     data = list(csv.reader(file))
     del data[0]
 
     data_ = list(map(friends._make, data))
+    data_ = sorted(data_, key=lambda x: dt.strptime(x[2]+x[3], '%d.%m.%Y%H:%M'))
 
-    print(data_)
+    for i in data_:
+        print(f"{i.surname} {i.name}")

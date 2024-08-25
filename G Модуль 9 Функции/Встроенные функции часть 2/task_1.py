11ed09409dcabd2e52696f706a80f34937bd1015
11fre <11free11@mail.ru>
2024-08-26 00:12:09 +0300

    task func

--- G Модуль 9 Функции/Встроенные функции часть 2/task_1.py
+++ G Модуль 9 Функции/Встроенные функции часть 2/task_1.py
@@ -3,6 +3,9 @@ def hash_as_key(d):
     for el in d:
         res[hash(el)] = res.get(hash(el), []) + [el]
     for key, value in res.items():
-        if len(value) > 1: res_[key] = value
-        else: res_[key] = value[0]
+        if len(value) > 1:
+            res_[key] = value
+        else:
+            res_[key] = value[0]
     return res_
+

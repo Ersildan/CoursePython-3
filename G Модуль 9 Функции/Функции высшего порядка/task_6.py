ef47634039da2d1b11d597b258cc727d20e2d44a
11fre <11free11@mail.ru>
2024-09-02 10:26:40 +0300

    task func

--- G Модуль 9 Функции/Функции высшего порядка/task_6.py
+++ G Модуль 9 Функции/Функции высшего порядка/task_6.py
@@ -1,34 +1,27 @@
 def verification(login, password, success, failure):
     ALF = "abcdefghijklmnopqrstuvwxyz"
+    if any([i.isalpha() and i.lower() in ALF for i in password]): flag1 = True
+    else: flag1 = False
 
-    if any([i.isalpha() and i.lower() in ALF for i in password]):
-        flag1 = True
-    else:
-        flag1 = False
+    if any([i in ALF.upper() for i in password]): flag2 = True
+    else: flag2 = False
 
-    if any([i in ALF.upper() for i in password]):
-        flag2 = True
-    else:
-        flag2 = False
+    if any([i in ALF for i in password]): flag3 = True
+    else: flag3 = False
 
-    if any([i in ALF for i in password]):
-        flag3 = True
-    else:
-        flag3 = False
-
-    if any([i.isdigit() for i in password]):
-        flag4 = True
-    else:
-        flag4 = False
+    if any([i.isdigit() for i in password]): flag4 = True
+    else: flag4 = False
 
     lst = [flag1, flag2, flag3, flag4]
     total = 0
+
     for i in lst:
         if i == False:
             key = total
             break
         else:
             total += 1
+
     d = {0: 'в пароле нет ни одной буквы',
          1: 'в пароле нет ни одной заглавной буквы',
          2: 'в пароле нет ни одной строчной буквы',

3198c64776072ce0f4a86bd8df81c90a46c405ce
11fre <11free11@mail.ru>
2024-12-21 11:23:41 +0300

    infinity cycle

--- I Модуль 10 Итераторы и генераторы/task_17.py
+++ I Модуль 10 Итераторы и генераторы/task_17.py
@@ -1,10 +1,19 @@
 class Cycle:
     def __init__(self, iterable):
         self.interable = iterable
+        self.inter_obj = iter(el for el in self.interable)
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        k = next(self.interable)
-        pass
+        try:
+            return next(self.inter_obj)
+        except:
+            self.inter_obj = iter(el for el in self.interable)
+            return next(self.inter_obj)
+
+
+cycle = Cycle([1])
+
+print(next(cycle) + next(cycle) + next(cycle))

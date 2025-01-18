071d02b6c1285f4cd37e08c40d075a811ee1dfa7
11fre <11free11@mail.ru>
2025-01-18 12:42:09 +0300

    work with generate

---
+++ I Модуль 10 Итераторы и генераторы/Задачи на Генераторы/task_32.py
@@ -0,0 +1,7 @@
+def parse_ranges(ranges):
+    split_comma_ranges = lambda s: (r.split('-') for r in s.split(','))
+    generate_ranges = lambda bounds: (range(int(start), int(end) + 1) for start, end in bounds)
+    return (j for i in generate_ranges(split_comma_ranges(ranges)) for j in i)
+
+
+print(*parse_ranges('12-14'))

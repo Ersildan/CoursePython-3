8580b229b40a96848972314a9a401591a9e52706
11fre <11free11@mail.ru>
2025-01-25 20:10:44 +0300

    +1

--- I Модуль 10 Итераторы и генераторы/Задачи на Генераторы/task_33.py
+++ I Модуль 10 Итераторы и генераторы/Задачи на Генераторы/task_33.py
@@ -1,6 +1,9 @@
 def filter_names(names, ignor_char, max_names):
     lst = (i for i in names if i.lower()[0] != ignor_char.lower() and any(j.isdigit() for j in i) == False)
-    pass
+    yield from (el for ind, el in enumerate(lst) if ind < max_names)
+
+
+
 
 data = ['Dima', 'Timur', 'Arthur', 'Anri20', 'Arina', 'German', 'Ruslan']
 

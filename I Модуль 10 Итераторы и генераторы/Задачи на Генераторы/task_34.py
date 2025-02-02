7374f4cb99213023b1d0bf8bbe39754d99f41dc6
11fre <11free11@mail.ru>
2025-02-02 12:49:36 +0300

    +1

--- I Модуль 10 Итераторы и генераторы/Задачи на Генераторы/task_34.py
+++ I Модуль 10 Итераторы и генераторы/Задачи на Генераторы/task_34.py
@@ -1,10 +1,8 @@
 from csv import DictReader
 
 def func(company):
-    if company['round'] == 'a':
-        return int(company['raisedAmt'])
-    else:
-        return 0
+    if company['round'] == 'a': return int(company['raisedAmt'])
+    else: return 0
 
 with open('data.csv', encoding='UTF-8') as file:
     data = DictReader(file)

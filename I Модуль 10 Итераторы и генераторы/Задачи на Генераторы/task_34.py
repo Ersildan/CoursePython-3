bfc143b8fb5a754947f8ed81d026f7622b23ece5
11fre <11free11@mail.ru>
2025-02-02 12:48:01 +0300

    +1

--- I Модуль 10 Итераторы и генераторы/Задачи на Генераторы/task_34.py
+++ I Модуль 10 Итераторы и генераторы/Задачи на Генераторы/task_34.py
@@ -1,1 +1,11 @@
-text = 'boom'
+from csv import DictReader
+
+def func(company):
+    if company['round'] == 'a':
+        return int(company['raisedAmt'])
+    else:
+        return 0
+
+with open('data.csv', encoding='UTF-8') as file:
+    data = DictReader(file)
+    print(sum(func(el) for el in data))

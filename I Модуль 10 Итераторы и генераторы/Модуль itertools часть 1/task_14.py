a88bd3946e7af414ff1a3060dad8b4ec1c9663c1
11fre <11free11@mail.ru>
2025-02-17 19:08:20 +0300

    Add decorator
    Decorator return values function factorials(n) - 1

--- I Модуль 10 Итераторы и генераторы/Модуль itertools часть 1/task_14.py
+++ I Модуль 10 Итераторы и генераторы/Модуль itertools часть 1/task_14.py
@@ -1,6 +1,16 @@
 from itertools import accumulate
 import operator
+import functools
 
+
+def decorator(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        value = func(*args, **kwargs)
+        return (i-1 for i in value)
+    return wrapper
+
+@decorator
 def factorials(n):
     return accumulate(range(1, n + 1), operator.mul)
 

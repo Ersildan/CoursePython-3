ea88b5651154dadc290234de067774f7b6f49e9a
11fre <11free11@mail.ru>
2025-02-18 14:51:24 +0300

    +1

---
+++ I Модуль 10 Итераторы и генераторы/Модуль itertools часть 2/task_1.py
@@ -0,0 +1,12 @@
+from itertools import dropwhile
+
+
+# Как только будет получено ложное значение, сгенерирует элементы из итерируемого объекта
+numbers = [1, 1, 2, 3, 4, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3]
+words = ['is', 'an', 'of', 'python', 'C#', 'beegeek', 'is']
+
+new_numbers = list(dropwhile(lambda num: num <= 5, numbers))
+print(new_numbers)
+
+for word in dropwhile(lambda s: len(s) == 2, words):
+    print(word)

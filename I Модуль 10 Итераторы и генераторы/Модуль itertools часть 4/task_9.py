a29628820bd416ebaeca1826a18b0fb1469f1988
11fre <11free11@mail.ru>
2025-04-20 10:58:06 +0300

    Practic groupby
    Use namedtuple

--- I Модуль 10 Итераторы и генераторы/Модуль itertools часть 4/task_9.py
+++ I Модуль 10 Итераторы и генераторы/Модуль itertools часть 4/task_9.py
@@ -1,5 +1,16 @@
-n = 1000
+from collections import namedtuple
+from itertools import groupby
 
-while n > 0:
-    print(n-7)
-    n = n - 7
+Person = namedtuple('Person', ['name', 'age', 'height'])
+
+persons = [Person('Tim', 63, 193), Person('Eva', 47, 158),
+           Person('Mark', 71, 172), Person('Alex', 45, 193),
+           Person('Jeff', 63, 193), Person('Ryan', 41, 184),
+           Person('Ariana', 28, 158), Person('Liam', 69, 193)]
+
+gr = groupby(sorted(
+    persons, key=lambda x: (x.height, x.name)),
+             key=lambda x: x.height)
+
+for k, v in gr:
+    print(f"{k}:", ", ".join([k.name for k in v]))

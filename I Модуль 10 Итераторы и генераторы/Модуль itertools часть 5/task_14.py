6ef180a36730decbec4f399ab31e1e9b86123895
11fre <11free11@mail.ru>
2025-05-29 15:33:02 +0300

    Use product
    input() n and m

--- I Модуль 10 Итераторы и генераторы/Модуль itertools часть 5/task_14.py
+++ I Модуль 10 Итераторы и генераторы/Модуль itertools часть 5/task_14.py
@@ -1,3 +1,11 @@
-def func():
-    ...
+from itertools import product
 
+numbers = (
+           '0', '1', '2', '3', '4', '5', '6',
+           '7', '8', '9', 'A', 'B', 'C', 'D',
+           'E', 'F'
+           )
+
+n, m = int(input()), int(input())
+for el in product(numbers[:n], repeat = m):
+    print("".join(el), end=' ')

1e8d122256dfebc70449a9b1950bc996bb213e11
11fre <11free11@mail.ru>
2025-06-01 10:15:16 +0300

    Create regex

---
+++ J Модуль 11 Регулярные выражения/task_12.py
@@ -0,0 +1,7 @@
+regex = r'[A-Z]{5}[0-9]{4}[A-Z]'
+
+# input() => The PAN (or PAN number) is a ten-character long alpha-numeric unique identifier. Example: AAAPZ1234C
+# output() => AAAPZ1234C
+
+# input() => first number is ABCD EZZPA1234ZaPMQ0000O, check thusEZZPA1234ZAPMQ0000O,
+# output() => EZZPA1234Z EZZPA1234Z

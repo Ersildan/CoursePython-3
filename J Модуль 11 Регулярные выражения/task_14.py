f3a067e7e5100be7e27c2b9bbffdb1af8a54a499
11fre <11free11@mail.ru>
2025-06-01 10:32:57 +0300

    Create regex

---
+++ J Модуль 11 Регулярные выражения/task_14.py
@@ -0,0 +1,7 @@
+regex = r'[a-z]{0,3}\d{2,8}[A-Z]{3,}'
+
+# input() => Dear citizen! Your old ID: tba44891AHH, your new ID: 781AHHGYT
+# output => tba44891AHH 781AHHGYT
+
+# input() => 1. name Tobot id: 234AZXR, 2. name Alph id: a6578ALPH, 3. name Teta id: abra0909CADABRA 4. name Alph up id: A6578ALPH
+# output => 234AZXR a6578ALPH bra0909CADABRA 6578ALPH

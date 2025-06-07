18f3a71fadf1efd5d89aeb25fd67d9531e56b42d
11fre <11free11@mail.ru>
2025-06-07 10:12:16 +0300

    +1

---
+++ J Модуль 11 Регулярные выражения/task_24.py
@@ -0,0 +1,152 @@
+regex = r'^(Mr|Mrs|Ms|Dr|Er)\.[A-Za-z]+$'
+
+'''
+# TEST_1:
+Mr.Guev
+
+# TEST_2:
+Ms.Jones
+
+# TEST_3:
+Dr. Guev
+
+# TEST_4:
+MRS.Traveler
+
+# TEST_5:
+Dear Mr.Tobot
+
+# TEST_6:
+Mrs.Jhonson
+
+# TEST_7:
+Dr.Patrick
+
+# TEST_8:
+Er.Paier
+
+# TEST_9:
+Mr.PaMrs.PaDr.Jhon
+
+# TEST_10:
+dr.Pedro
+
+# TEST_11:
+DDr.Pedro
+
+# TEST_12:
+mr.checked
+
+# TEST_13:
+ms.foo
+
+# TEST_14:
+Mr.X
+
+# TEST_15:
+Mr.
+
+# TEST_16:
+Dr.
+
+# TEST_17:
+Mrs.
+
+# TEST_18:
+Er.
+
+# TEST_19:
+Mss.Timur
+
+# TEST_20:
+Mrr.Timur
+
+# TEST_21:
+Err.Timur
+
+# TEST_22:
+M.Timur
+
+# TEST_23:
+Es.Timur
+
+# TEST_24:
+Ds.Timur
+
+# TEST_25:
+MrWolf'''
+
+'''
+# TEST_1:
+Mr.Guev
+
+# TEST_2:
+Ms.Jones
+
+# TEST_3:
+
+
+# TEST_4:
+
+
+# TEST_5:
+
+
+# TEST_6:
+Mrs.Jhonson
+
+# TEST_7:
+Dr.Patrick
+
+# TEST_8:
+Er.Paier
+
+# TEST_9:
+
+
+# TEST_10:
+
+
+# TEST_11:
+
+
+# TEST_12:
+
+
+# TEST_13:
+
+
+# TEST_14:
+Mr.X
+
+# TEST_15:
+
+
+# TEST_16:
+
+
+# TEST_17:
+
+
+# TEST_18:
+
+
+# TEST_19:
+
+
+# TEST_20:
+
+
+# TEST_21:
+
+
+# TEST_22:
+
+
+# TEST_23:
+
+
+# TEST_24:
+
+
+# TEST_25'''

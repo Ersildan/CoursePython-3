41298c493323d63a31d0710a96eb237ad2eeefc5
11fre <11free11@mail.ru>
2025-06-20 21:42:33 +0300

    +1

--- k Модуль re/task_5.py
+++ k Модуль re/task_5.py
@@ -6,4 +6,4 @@ for el in sys.stdin.read().splitlines():
         match = search(r'(\d{1,3})([ -])(\d{1,3})\2(\d{4,10})', el)
         print(f'Код страны: {match.group(1)}, Код города: {match.group(3)}, Номер: {match.group(4)}')
     except:
-        None
+        ...

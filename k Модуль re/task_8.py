41758f9e3e2b2e31583214ad22d821f156e874a0
11fre <11free11@mail.ru>
2025-06-25 09:46:57 +0300

    New code
    Use search from re

--- k Модуль re/task_8.py
+++ k Модуль re/task_8.py
@@ -1,3 +1,20 @@
-import re
+from re import search
 
-...
+t1 = t2 = 0
+for el in open(0):
+    if search(r'bee.*bee', el.rstrip('\n')): t1 += 1
+    if search(r'\bgeek\b', el.rstrip('\n')): t2 += 1
+print(t1, t2, sep='\n')
+
+'''Напишите программу, определяющую:
+
+количество строк, в которых bee встречается в качестве подстроки не менее двух раз
+количество строк, в которых geek встречается в качестве слова хотя бы один раз'''
+
+#test1
+'''
+beebee bee
+beegeek
+bee geek bee'''
+# 2 - Первый паттерн
+# 1 - Второй паттерн
